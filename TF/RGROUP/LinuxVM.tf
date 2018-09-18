b970cfdd49ddc11cf92784d7822224b47ba48bbb
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-18 22:28:39 +0530

    Update LinuxVM.tf

--- TF/RGROUP/LinuxVM.tf
+++ TF/RGROUP/LinuxVM.tf
@@ -5,11 +5,6 @@ resource "azurerm_resource_group" "main" {
   name     = "${var.resource_group}"
   location = "${var.location}"
 }
-
-data "azurerm_resource_group_name" "main" {
-  name                = "${azurerm_resource_group_name.main.name}"
-}
-
 output "ResourceGroup-ID" {
   value = "${data.azurerm_resource_group_name.main.id}"
 }
