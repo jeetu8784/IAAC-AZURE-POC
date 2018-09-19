507e1b1dd1a8f620be43cc4f01f2e314b56ac5b6
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-19 15:01:08 +0530

    Update LinuxVM.tf

--- TF/NIC/LinuxVM.tf
+++ TF/NIC/LinuxVM.tf
@@ -1,29 +1,15 @@
 provider "azurerm" { }
 
-data "azurerm_resource_group" "main" {
-  name                      = "${var.resource_group}"
-  }
-data "azurerm_network_security_group" "main" {
-  name                      = "${var.networkSecurityGroupName}"
-  }
-data "azurerm_subnet" "main" {
-  name                      = "${var.subnetName}"
-  }
-data "azurerm_public_ip" "main" {
-  name                      = "${var.publicIpAddressName}"
-  }
 # Create a network interface for VMs and attach the PIP and the NSG
 resource "azurerm_network_interface" "main" {
   name                      = "${var.networkInterfaceName}"
   location                  = "${var.location}"
-  resource_group_name       = "${data.azurerm_resource_group.main.id}"
-  network_security_group_id = "${data.azurerm_network_security_group.main.id}"
-
+  resource_group_name       = "${var.resource_group}"
+  
   ip_configuration {
     name                          = "ipconfig"
-    subnet_id                     = "${data.azurerm_subnet.main.id}"
+    subnet_id                     = "${var.defaultsubnetid}"
     private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = "${data.azurerm_public_ip.main.id}"
   }
 }
 output "NIC-ID" {
