7735b78ce4221e2b333e3c709d669f70b18df3f7
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-20 13:23:51 +0530

    Update LinuxVM.tf

--- TF/VMS/LinuxVM.tf
+++ TF/VMS/LinuxVM.tf
@@ -1,75 +1,18 @@
 provider "azurerm" { }
 
-# Create a Resource Group for the new Virtual Machine.
-resource "azurerm_resource_group" "main" {
-  name     = "${var.resource_group}"
-  location = "${var.location}"
-}
-
-# Create a Virtual Network within the Resource Group
-resource "azurerm_virtual_network" "main" {
-  name                = "${var.virtualNetworkName}"
-  address_space       = ["${var.addressPrefix}"]
-  resource_group_name = "${azurerm_resource_group.main.name}"
-  location            = "${azurerm_resource_group.main.location}"
-}
-
-# Create a Subnet within the Virtual Network
-resource "azurerm_subnet" "internal" {
-  name                 = "${var.subnetName}"
-  virtual_network_name = "${azurerm_virtual_network.main.name}"
-  resource_group_name  = "${azurerm_resource_group.main.name}"
-  address_prefix       = "${var.subnetPrefix}"
-}
-
 # Create a Public IP for the Virtual Machine
 resource "azurerm_public_ip" "main" {
   name                         = "${var.publicIpAddressName}"
-  location                     = "${azurerm_resource_group.main.location}"
-  resource_group_name          = "${azurerm_resource_group.main.name}"
+  location                     = "${var.location}"
+  resource_group_name          = "${var.resource_group}"
   public_ip_address_allocation = "${var.publicIpAddressType}"
-}
-
-# Create a Network Security Group with some rules
-resource "azurerm_network_security_group" "main" {
-  name                = "${var.networkSecurityGroupName}"
-  location            = "${azurerm_resource_group.main.location}"
-  resource_group_name = "${azurerm_resource_group.main.name}"
-
-  security_rule {
-    name                       = "allow_SSH"
-    description                = "Allow SSH access"
-    priority                   = 100
-    direction                  = "Inbound"
-    access                     = "Allow"
-    protocol                   = "Tcp"
-    source_port_range          = "*"
-    destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
-  }
-}
-
-# Create a network interface for VMs and attach the PIP and the NSG
-resource "azurerm_network_interface" "main" {
-  name                      = "${var.networkInterfaceName}"
-  location                  = "${azurerm_resource_group.main.location}"
-  resource_group_name       = "${azurerm_resource_group.main.name}"
-  network_security_group_id = "${azurerm_network_security_group.main.id}"
-
-  ip_configuration {
-    name                          = "ipconfig"
-    subnet_id                     = "${azurerm_subnet.internal.id}"
-    private_ip_address_allocation = "dynamic"
-    public_ip_address_id          = "${azurerm_public_ip.main.id}"
-  }
 }
 
 # Create a new Virtual Machine based on the Golden Image
 resource "azurerm_virtual_machine" "vm" {
   name                             = "${var.virtualMachineName}"
-  location                         = "${azurerm_resource_group.main.location}"
-  resource_group_name              = "${azurerm_resource_group.main.name}"
+  location                         = "${azurerm_public_ip.main.location}"
+  resource_group_name              = "${azurerm_public_ip.main.name}"
   network_interface_ids            = ["${azurerm_network_interface.main.id}"]
   vm_size                          = "${var.virtualMachineSize}"
   delete_os_disk_on_termination    = true
