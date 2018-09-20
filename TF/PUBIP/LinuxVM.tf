735a183b7863b887f0d16e83dca0d4dad4e1b644
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-20 13:37:17 +0530

    Update LinuxVM.tf

--- TF/PUBIP/LinuxVM.tf
+++ TF/PUBIP/LinuxVM.tf
@@ -2,7 +2,7 @@ provider "azurerm" { }
 
 # Create a Public IP for the Virtual Machine
 resource "azurerm_public_ip" "main" {
-  name                         = "${var.publicIpAddressNameNew}"
+  name                         = "${var.publicIpAddressName}"
   location                     = "${var.location}"
   resource_group_name          = "${var.resource_group}"
   public_ip_address_allocation = "${var.publicIpAddressType}"
