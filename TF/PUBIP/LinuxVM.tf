0455f30aacf063e034e62e43179d5a52b4bd78e4
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-20 13:25:43 +0530

    Update LinuxVM.tf

--- TF/PUBIP/LinuxVM.tf
+++ TF/PUBIP/LinuxVM.tf
@@ -7,7 +7,11 @@ resource "azurerm_public_ip" "main" {
   resource_group_name          = "${var.resource_group}"
   public_ip_address_allocation = "${var.publicIpAddressType}"
 }
+data "aws_network_interfaces" "main" {}
 
+output "example" {
+  value = "${data.aws_network_interfaces.main.ids}"
+}
 output "AzurePublicIP" {
   value = "${azurerm_public_ip.main.ip_address}"
 }
