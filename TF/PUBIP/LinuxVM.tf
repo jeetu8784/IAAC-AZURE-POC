a3db13b31746f4a2c260c15076ee46628cbfb31e
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-25 00:42:31 +0530

    Update LinuxVM.tf

--- TF/PUBIP/LinuxVM.tf
+++ TF/PUBIP/LinuxVM.tf
@@ -1,26 +1,13 @@
 provider "azurerm" { }
 
 
-data "azurerm_network_interface" "main" {
-  name                      = "${var.networkInterfaceName}"
-  resource_group_name       = "${var.resource_group}"
-  }
-
-output "example" {
-  value = "${data.azurerm_network_interface.main.id}"
-}
-
 # Create a Public IP for the Virtual Machine
 resource "azurerm_public_ip" "main" {
-  name                         = "HIJK"
+  name                         = "HIJKLM"
   location                     = "${var.location}"
   resource_group_name          = "${var.resource_group}"
   public_ip_address_allocation = "${var.publicIpAddressType}"
-  
-  tags{
-    environment = "${data.azurerm_network_interface.main.id}"
-    }
-}
+ }
 
 output "AzurePublicIP" {
   value = "${azurerm_public_ip.main.ip_address}"
