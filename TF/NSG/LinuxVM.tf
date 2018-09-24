065d2d57cd8c2f975361279abc27e6500a69f10e
Siddhartha-Rastogi <siddhartha.rastogi@gmail.com>
2018-09-24 11:42:02 +0530

    Update LinuxVM.tf

--- TF/NSG/LinuxVM.tf
+++ TF/NSG/LinuxVM.tf
@@ -16,11 +16,6 @@ resource "azurerm_network_security_group" "sshnsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-}
-resource "azurerm_network_security_group" "httpnsg" {
-  name                = "${var.httpnetworkSecurityGroupName}"
-  location            = "${var.location}"
-  resource_group_name = "${var.resource_group}"
   security_rule {
     name                       = "allow_HTTP"
     description                = "Allow HTTP access"
@@ -37,6 +32,4 @@ resource "azurerm_network_security_group" "httpnsg" {
 output "HTTPNSG-ID" {
   value = "${azurerm_network_security_group.httpnsg.id}"
 }
-output "SSHNSG-ID" {
-  value = "${azurerm_network_security_group.sshnsg.id}"
-}
+
